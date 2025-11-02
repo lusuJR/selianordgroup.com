f6ea4a1681eaf067a0c7a56bf2051b585d1e4e03
Selemani <lusukama.s@gmail.com>
2025-11-02 17:52:06 +0200

    configure SQL Server connection and register ApplicationDbContext in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using selianordgroup.com.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Register ApplicationDbContext with SQL Server connection string
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
